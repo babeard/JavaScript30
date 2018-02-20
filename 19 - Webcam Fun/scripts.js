6cc56bb2adc40a4f3d7e41b1a7fef63e9986af65
Brian Beard <brian.a.beard@gmail.com>
2018-02-20 11:56:51 -0600

    19 completed - fix takePhoto() performance

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -7,21 +7,28 @@ const snap = document.querySelector('.snap');
 navigator.mediaDevices.getUserMedia({ video: { width: canvas.offsetWidth, height: canvas.offsetHeight } })
     .then(stream => {
 
+        // Source the stream
         video.srcObject = stream
 
-        video.addEventListener('loadedmetadata', (e) => {            
+        video.addEventListener('loadedmetadata', (e) => {
 
+            // Play the video
             video.play()
 
+            // Get the video width
+            canvas.width = video.videoWidth
+            canvas.height = video.videoHeight
+
+            // Loop every 10 ms
             setInterval(() => {
 
-                canvas.width = video.videoWidth
-                canvas.height = video.videoHeight
-
+                // Draw the image to the canvas
                 ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
 
+                // Retrieve pixel information
                 const data = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
+                // Manipulate data and put back on cavas
                 ctx.putImageData(rgbSplit(data), 0, 0)
 
             }, 10)
@@ -37,9 +44,22 @@ function takePhoto() {
     // Grab the canvas data
     const data = canvas.toDataURL()
 
-    // Add to the photo strip and a downloadable link
-    strip.innerHTML += `<a download='download.png' href='${data}'><img src='${data}' /></a>`
+    // Create the needed HTML elements
+    const a = document.createElement('a')
+    const img = document.createElement('img')
 
+    // Set needed attributes
+    a.setAttribute('download', 'download.png')
+    a.setAttribute('href', data)
+    img.setAttribute('src', data)
+
+    // Append
+    a.appendChild(img)
+    strip.appendChild(a)
+
+    // Play audio
+    snap.currentTime = 0
+    snap.play()
 }
 
 /* Cheated on below function */
